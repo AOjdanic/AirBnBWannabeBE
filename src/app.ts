1620db02e26766a72842d1b92cee49f081639f25
Aleksa Ojdanic <aojdanic97@gmail.com>
2024-08-29 13:58:19 +0200

    add eslint and prettier to project

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-import express, { Request, Response } from "express";
-import morgan from "morgan";
+import express, { Request, Response } from 'express';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(express.json());
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
-app.get("/", (_: Request, res: Response) => {
+app.get('/', (_: Request, res: Response) => {
   res.status(200).json({
-    message: "Hello There. General Kenobi! You are a bold one",
+    message: 'Hello There. General Kenobi! You are a bold one',
   });
 });
 
