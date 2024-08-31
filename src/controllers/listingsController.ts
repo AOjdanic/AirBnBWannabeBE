7510cc0687573c6f966c5940f012033e4fac6f45
Aleksa Ojdanic <aojdanic97@gmail.com>
2024-08-31 19:04:00 +0200

    connected to db, and queried all listings | listings by id don't seem to work

--- src/controllers/listingsController.ts
+++ src/controllers/listingsController.ts
@@ -1,15 +1,24 @@
 import { Request, Response } from 'express';
+import Listing from '../models/Listings';
 
-export const getAllListings = (_: Request, res: Response) => {
+export const getAllListings = async (_: Request, res: Response) => {
+  const listings = await Listing.find().limit(30);
   res.status(200).json({
     message: 'These are all listings',
+    data: {
+      listings,
+    },
   });
 };
 
-export const getListing = (req: Request, res: Response) => {
+export const getListing = async (req: Request, res: Response) => {
   const { id } = req.params;
+  const listing = await Listing.findById(id); // doesn't work
+
   res.status(200).json({
     message: `This is only one listing with id ${id}`,
-    id,
+    data: {
+      listing,
+    },
   });
 };
