2f594d99a1ec11b1326670ea75e9f21bb8b84319
Aleksa Ojdanic <aojdanic97@gmail.com>
2024-09-21 11:49:26 +0200

    implement rest of the crud route handlers

--- src/controllers/listingsController.ts
+++ src/controllers/listingsController.ts
@@ -4,9 +4,20 @@ import Listing from '../models/Listings';
 export const getAllListings = async (_: Request, res: Response) => {
   const listings = await Listing.find().limit(30);
   res.status(200).json({
-    message: 'These are all listings',
+    message: 'success',
     data: {
       listings,
+    },
+  });
+};
+
+export const createListing = async (req: Request, res: Response) => {
+  const listing = await Listing.create(req.body);
+
+  return res.status(201).json({
+    message: 'success',
+    data: {
+      listing,
     },
   });
 };
@@ -17,9 +28,35 @@ export const getListing = async (req: Request, res: Response) => {
   const listing = await Listing.findById(id);
 
   res.status(200).json({
-    message: `This is only one listing with id ${id}`,
+    message: 'success',
     data: {
       listing,
     },
+  });
+};
+
+export const updateListing = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const updatedListing = await Listing.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    message: 'success',
+    data: {
+      listing: updatedListing,
+    },
+  });
+};
+
+export const deleteListing = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  await Listing.findByIdAndDelete(id);
+
+  res.status(204).json({
+    message: 'success',
   });
 };
--- src/routes/listings.ts
+++ src/routes/listings.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 
-import { getAllListings, getListing } from '../controllers/listingsController';
+import {
+  createListing,
+  deleteListing,
+  getAllListings,
+  getListing,
+  updateListing,
+} from '../controllers/listingsController';
 
 const router = express.Router();
 
-router.route('/').get(getAllListings);
+router.route('/').get(getAllListings).post(createListing);
 
-router.route('/:id').get(getListing);
+router.route('/:id').get(getListing).delete(deleteListing).patch(updateListing);
 
 export default router;
