b583b81d45d9a527de47fc75b683f9b37ad14dcd
Aleksa Ojdanic <aojdanic97@gmail.com>
2024-09-21 22:52:37 +0200

    handle uncaught exceptions and unhandled rejections in the entire app

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
+/* eslint-disable no-console */
 import app from './app';
 import mongoose from 'mongoose';
+
+process.on('uncaughtException', (err) => {
+  console.log('uncaught exception');
+  console.log(err);
+  process.exit(1);
+});
 
 const uri =
   process.env.DATABASE_URI?.replace(
@@ -9,8 +16,14 @@ const uri =
 
 mongoose.connect(uri).then(() => console.log('Database connection sucessful'));
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(
     `Starting server on port ${process.env.PORT}. Listening on http://localhost:${process.env.PORT}`,
   );
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('unhandled rejection error');
+  console.log(err);
+  server.close(() => process.exit(1));
 });
