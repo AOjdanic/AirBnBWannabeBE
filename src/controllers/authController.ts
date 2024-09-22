103b251f634d312172762c7bfc3aa97e7f235c0e
Aleksa Ojdanic <aojdanic97@gmail.com>
2024-09-23 01:17:57 +0200

    update model name to point at right collection | remove check for already existing user as schema takes care of that

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,14 +1,8 @@
 import User from '../models/Users';
-import { AppError } from '../utils/AppError';
 import { catchAsyncErrors } from '../utils/catchAsyncErrors';
 
-export const signup = catchAsyncErrors(async (req, res, next) => {
+export const signup = catchAsyncErrors(async (req, res) => {
   const { email, password, passwordConfirm, name } = req.body;
-
-  const alreadyExistingUser = await User.findOne({ email });
-  if (alreadyExistingUser) {
-    return next(new AppError('The user with this email already exists', 400));
-  }
 
   const newUser = await User.create({
     name,
@@ -18,7 +12,7 @@ export const signup = catchAsyncErrors(async (req, res, next) => {
   });
 
   res.status(200).json({
-    message: 'ethentuihent',
+    message: 'success',
     data: {
       user: newUser,
     },
